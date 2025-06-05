abe92e5f8771bb532f7f44dc12604038a6cadb29
DAWIT2394 <davebireda2394@gmail.com>
2025-06-05 17:06:33 +0300

    work

--- src/components/CustomerReview.js
+++ src/components/CustomerReview.js
@@ -23,7 +23,9 @@ function CustomerReview() {
                                         </div>
                                         <h5 className="mt-4 mb-0"><strong className="text-warning text-uppercase">Paul Mitchel</strong></h5>
                                         <h6 className="text-dark m-0">Web Developer</h6>
-                                        <p className="m-0 pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui.</p>
+                                        <p className="m-0 pt-3">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui.</p>
                                     </div>
                                     <div className="carousel-item text-center">
                                         <div className="img-box p-1 border rounded-circle m-auto">
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ import 'slick-carousel/slick/slick-theme.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../components/Home.css'
+import CustomerReview from "./CustomerReview";
+
 export default function Home() {
   const [activeTab, setActiveTab] = useState("all");
 
@@ -59,11 +61,11 @@ export default function Home() {
     },
     {
       img: '/images/slider-02.jpg',
-      title: 'Eathiopian Restaurant',
+      title: 'Welcome To Eathiopian Restaurant',
     },
     {
       img: '/images/slider-03.jpg',
-      title: 'Eathiopian Restaurant',
+      title: 'Welcome To Eathiopian Restaurant',
     },
   ];
 
@@ -109,7 +111,7 @@ export default function Home() {
                 <div className="row">
                   <div className="col-md-12">
                     <h1 className="m-b-20">
-                      <strong>Welcome To <br /> {slide.title}</strong>
+                      <strong> <br /> {slide.title}</strong>
                     </h1>
                     <p className="m-b-40">
                       See how your users experience your website in realtime or view <br /> 
@@ -127,7 +129,6 @@ export default function Home() {
           ))}
         </Slider>
       </div>
-{/* Menu */}
 
       {/* About Section */}
       <div className="about-section-box py-5">
@@ -172,6 +173,138 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* Menu */}
+<div className="qt-box qt-background">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-8 ml-auto mr-auto text-center">
+					<p className="<lead> ">
+						" If you're not the one cooking, stay out of the way and compliment the chef. "
+					</p>
+					<span className="lead">Michael Strahan</span>
+				</div>
+			</div>
+		</div>
+	</div>
+<div>
+
+
+      <div className="menu-box">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="heading-title text-center">
+                <h2>Special Menu</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row inner-menu-box">
+          <div className="col-3">
+  <div className="nav flex-column nav-pills">
+    <button
+      style={{ border: "none" }}
+      className={`nav-link mb-2 ${activeTab === "all" ? "active" : ""}`}
+      onClick={() => setActiveTab("all")}
+    >
+      All
+    </button>
+    <button
+      style={{ border: "none" }}
+      className={`nav-link mb-2 ${activeTab === "drinks" ? "active" : ""}`}
+      onClick={() => setActiveTab("drinks")}
+    >
+      Drinks
+    </button>
+    <button
+      style={{ border: "none" }}
+      className={`nav-link mb-2 ${activeTab === "lunch" ? "active" : ""}`}
+      onClick={() => setActiveTab("lunch")}
+    >
+      Lunch
+    </button>
+    <button
+      style={{ border: "none" }}
+      className={`nav-link mb-2 ${activeTab === "dinner" ? "active" : ""}`}
+      onClick={() => setActiveTab("dinner")}
+    >
+      Dinner
+    </button>
+  </div>
+</div>
+
+
+
+
+            <div className="col-9">
+              <div className="tab-content">
+                <div className="tab-pane fade show active">
+                  <div className="row">
+                    {renderItems(activeTab)}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    {/* Gallery */}
+    <div className="gallery-box">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="heading-title text-center">
+                                <h2>Gallery</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="tz-gallery">
+                        <div className="row">
+                            <div className="col-sm-12 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-01.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-01.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-02.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-02.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-03.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-03.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                            <div className="col-sm-12 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-04.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-04.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-05.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-05.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                            <div className="col-sm-6 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-06.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-06.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                             <div className="col-sm-6 col-md-4 col-lg-4">
+                                <a className="lightbox" href="images/gallery-img-06.jpg">
+                                    <img className="img-fluid" src="images/gallery-img-06.jpg" alt="Gallery Images" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <CustomerReview/>
+
     </div>
   );
 }
